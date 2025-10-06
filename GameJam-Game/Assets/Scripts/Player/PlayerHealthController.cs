d46ec6e67334ce1c9dd011030c6bb55dcc364318
Peat <hendy123@gmx.de>
2025-10-06 21:19:26 +0200

    integrate player dying and game over per player death

---
+++ GameJam-Game/Assets/Scripts/Player/PlayerHealthController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Nidavellir.Entity;
+using Nidavellir.EventArgs;
+using Nidavellir.GameEventBus;
+using Nidavellir.GameEventBus.Events;
+using Nidavellir.Scriptables;
+using UnityEngine;
+
+namespace Nidavellir.Player
+{
+    public class PlayerHealthController : MonoBehaviour
+    {
+        [SerializeField] private EntityStats m_entityStats;
+        [SerializeField] private CharacterStatFacade m_characterStatFacade;
+
+        private void Awake()
+        {
+            this.m_entityStats ??= FindFirstObjectByType<EntityStats>(FindObjectsInactive.Include);
+        }
+
+        private void Start()
+        {
+            this.m_entityStats[this.m_characterStatFacade.Hp].OnValueChanged += this.OnHpChanged;
+        }
+
+        private void OnHpChanged(object sender, CharacterStatValueChangeEventArgs e)
+        {
+            if (e.NewValue > 0)
+                return;
+
+            GameEventBus<PlayerDiedEvent>.Invoke(this, new());
+        }
+    }
+}
