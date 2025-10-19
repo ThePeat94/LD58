c428df559d0144aeef5ccb6442622573245bc30d
Peat <hendy123@gmx.de>
2025-10-20 01:40:01 +0200

    improve game event bus with priority and sorted list

--- GameJam-Game/Assets/Scripts/EventBus/GameEventBus.cs
+++ GameJam-Game/Assets/Scripts/EventBus/GameEventBus.cs
@@ -1,28 +1,54 @@
+using System;
 using System.Collections.Generic;
 using Nidavellir.EventBus.EventBindings;
 using Nidavellir.EventBus.Events;
+using UnityEngine;
 
 namespace Nidavellir.EventBus
 {
     public static class GameEventBus<T> where T : IEvent
     {
-        private static readonly HashSet<IEventBinding<T>> s_eventBindings = new();
-    
-        public static void Register(IEventBinding<T> eventBinding)
+        private static readonly SortedList<int, HashSet<IEventBinding<T>>> s_sortedEventBindings = new();
+        
+        public static void Register(IEventBinding<T> eventBinding, int priority = 0)
         {
-            s_eventBindings.Add(eventBinding);
+            if (!s_sortedEventBindings.ContainsKey(priority))
+            {
+                s_sortedEventBindings[priority] = new HashSet<IEventBinding<T>>();
+            }
+            
+            s_sortedEventBindings[priority].Add(eventBinding);
         }
-    
+
         public static void Unregister(IEventBinding<T> eventBinding)
         {
-            s_eventBindings.Remove(eventBinding);
+            foreach (var bindingsSet in s_sortedEventBindings.Values)
+            {
+                if (bindingsSet.Remove(eventBinding))
+                {
+                    break;
+                }
+            }
         }
     
         public static void Invoke(object sender, T args)
         {
-            foreach (var eventBinding in s_eventBindings)
+            var bindingsCopy = new SortedDictionary<int, HashSet<IEventBinding<T>>>(s_sortedEventBindings);
+            foreach (var (prio, eventBindings) in bindingsCopy)
             {
-                eventBinding.Invoke(sender, args);
+                Debug.Log($"Invoking event bindings with priority {prio} for event {typeof(T).Name}");
+                foreach (var eventBinding in eventBindings)
+                {
+                    try
+                    {
+                        eventBinding.Invoke(sender, args);
+                    } 
+                    catch (Exception ex)
+                    {
+                        Debug.LogError($"Error invoking event binding: {ex}");
+                    }
+                }
+
             }
         }
     }
